c0a39270a6f5483090fcd95c1de78a5074713226
minionsrahat <rahatuddin786@gmail.com>
2023-04-28 12:42:34 +0600

    New Resume Parser Implemented Using Spacy

--- resume_screening/resume_parser.py
+++ resume_screening/resume_parser.py
@@ -3,16 +3,71 @@ import docx2txt
 import PyPDF2
 import spacy
 from spacy.matcher import PhraseMatcher
+from spacy.lang.en import English
 # load default skills data base
 from skillNer.general_params import SKILL_DB
 # import skill extractor
 from skillNer.skill_extractor_class import SkillExtractor
 # init params of skill extractor
 nlp = spacy.load("en_core_web_lg")
-
 import os
 skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher)
-outdir = './Dataset/Resume files'
+outdir = './Dataset'
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+
+
+def match_majors_by_spacy(job):
+        majors_patterns_path = os.path.join(outdir, 'Resources/data/majors.jsonl') 
+        nlp = English()
+        # Add the pattern to the matcher
+        patterns_path = majors_patterns_path
+        ruler = nlp.add_pipe("entity_ruler")
+        ruler.from_disk(patterns_path)
+        # Process some text
+        doc1 = nlp(job)
+        acceptable_majors = []
+        for ent in doc1.ents:
+            labels_parts = ent.label_.split('|')
+            if labels_parts[0] == 'MAJOR':
+                if labels_parts[2].replace('-', ' ') not in acceptable_majors:
+                    acceptable_majors.append(labels_parts[2].replace('-', ' '))
+                if labels_parts[2].replace('-', ' ') not in acceptable_majors:
+                    acceptable_majors.append(labels_parts[2].replace('-', ' '))
+        return acceptable_majors
+
+def match_degrees_by_spacy(job):
+        degrees_patterns_path = os.path.join(outdir, 'Resources/data/degrees.jsonl') 
+        nlp = English()
+        # Add the pattern to the matcher
+        patterns_path = degrees_patterns_path
+        ruler = nlp.add_pipe("entity_ruler")
+        ruler.from_disk(patterns_path)
+        # Process some text
+        doc1 = nlp(job)
+        degree_levels = []
+        for ent in doc1.ents:
+            labels_parts = ent.label_.split('|')
+            if labels_parts[0] == 'DEGREE':
+                if labels_parts[1] not in degree_levels:
+                    degree_levels.append(labels_parts[1])
+        return degree_levels
+
+def match_skills_by_spacy(job):
+        skills_patterns_path =os.path.join(outdir, 'Resources/data/skills.jsonl') 
+        nlp = English()
+        patterns_path = skills_patterns_path
+        ruler = nlp.add_pipe("entity_ruler")
+        ruler.from_disk(patterns_path)
+        # Process some text
+        doc1 = nlp(job)
+        job_skills = []
+        for ent in doc1.ents:
+            labels_parts = ent.label_.split('|')
+            if labels_parts[0] == 'SKILL':
+                if labels_parts[1].replace('-', ' ') not in job_skills:
+                    job_skills.append(labels_parts[1].replace('-', ' '))
+        return ','.join(str(x) for x in job_skills)
 
 def extract_attributes(data):
     skills=(','.join(word for word in data['skills']))
@@ -54,7 +109,7 @@ def get_sub_matches(annotations):
             skills.add(dict_item['doc_node_value'])  
     return skills
 
-def extract_skills(des):
+def extract_skills_by_extractor(des):
     try:
         annotations = skill_extractor.annotate(des)
         full_match=get_full_match(annotations)
@@ -65,6 +120,13 @@ def extract_skills(des):
         return ','.join(str(x) for x in full_match)
     except:
         return " "
+
+def extract_skills_by_spacy(text):
+    resume={}
+    resume['majors']=(match_majors_by_spacy(text))
+    resume['degrees']=(match_degrees_by_spacy(text))
+    resume['skill']=(match_skills_by_spacy(text))
+    return resume;
 
 # Extract text from file based on file type
 def extract_text_from_file(file_path):
@@ -80,8 +142,8 @@ def extract_text_from_file(file_path):
 
 def parser(resume_file):
     print("Resume Name"+resume_file)
-    resume_text = extract_text_from_file(os.path.join(outdir,resume_file))
-    resume_skills=extract_skills(resume_text)
+    resume_text = extract_text_from_file(os.path.join(outdir,"Resume files/"+resume_file))
+    resume_skills=extract_skills_by_extractor(resume_text)
     return resume_skills
 
 def main():
